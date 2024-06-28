a321ec3217fa7fd2a729199aeb113e8c40138e1e
Ivan Trajkov <ivche@trajkov.mk>
2024-06-28 07:01:47 +0200

    Add index page.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,8 +3,51 @@
 </script>
 
 <main>
-  <h1>Candywatch</h1>
+  <h1 id="title">candywatch</h1>
+  <div id="menu">
+    <button class="button">CREATE ROOM</button>
+    <button class="button">JOIN ROOM</button>
+  </div>
 </main>
 
 <style>
+ main {
+   display: flex;
+   flex-direction: column;
+   text-align: center;
+ }
+
+ #title {
+   font-size: 3rem;
+ }
+
+ #menu {
+   gap: 10px;
+   display: flex;
+   padding: 30px;
+   flex-direction: column;
+   justify-content: space-around;
+   background-color: red;
+   min-width: 30vh;
+   min-height: 15vh;
+   border-radius: 25px;
+ }
+
+ .button {
+   min-width: 30px;
+   min-height: 40px;
+   border-radius: 25px;
+   border: 0;
+   background-color: white;
+   font-size: 1.3rem;
+ }
+
+ .button:hover {
+   background-color: grey;
+ }
+
+ .button:active {
+   scale: 0.95;
+ }
+
 </style>
